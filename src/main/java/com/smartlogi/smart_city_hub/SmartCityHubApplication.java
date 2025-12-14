b1612d1581c871622ba4eec97e4aa3352b502442
hamzalhadouchi <hamzabniss16@gmail.com>
2025-12-14 18:54:58 +0100

    configration IDE

---
+++ src/main/java/com/smartlogi/smart_city_hub/SmartCityHubApplication.java
@@ -0,0 +1,13 @@
+package com.smartlogi.smart_city_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCityHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCityHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartlogi/smart_city_hub/SmartCityHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlogi.smart_city_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
