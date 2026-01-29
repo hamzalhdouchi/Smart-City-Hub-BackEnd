94eebbbe82bc0118106d7de751a184db2b9b26a4
hamzalhadouchi <hamzabniss16@gmail.com>
2026-01-29 14:26:46 +0000

    add NotificationSimpleResponse DTO and NotificationType enum for notification handling

---
+++ src/main/java/com/smartlogi/smart_city_hub/entity/enums/NotificationType.java
@@ -0,0 +1,15 @@
+package com.smartlogi.smart_city_hub.entity.enums;
+
+/**
+ * Types of notifications that can be sent to users.
+ */
+public enum NotificationType {
+    INCIDENT_CREATED,
+    INCIDENT_ASSIGNED,
+    STATUS_CHANGED,
+    COMMENT_ADDED,
+    INCIDENT_RESOLVED,
+    RATING_REQUESTED,
+    SLA_EXCEEDED,
+    CRITICAL_INCIDENT
+}
