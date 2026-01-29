098fbe770756ca9c82feb53d6ff1e6ae3770fe74
hamzalhadouchi <hamzabniss16@gmail.com>
2026-01-29 14:26:55 +0000

    add OpenApiConfig for API documentation and security configuration

---
+++ src/main/java/com/smartlogi/smart_city_hub/config/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package com.smartlogi.smart_city_hub.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Smart City Hub API")
+                        .description("API for Smart City Hub - Urban Incident Management Platform")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Smart City Hub Team")
+                                .email("support@smartcityhub.com"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Enter JWT token")));
+    }
+}
