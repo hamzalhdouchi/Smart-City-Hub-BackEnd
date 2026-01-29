ef689bc4383691789b10ac0ca1fc750dd1edce56
hamzalhadouchi <hamzabniss16@gmail.com>
2026-01-29 14:33:33 +0000

    add Role enum for defining user roles in the Smart City Hub system

---
+++ src/main/java/com/smartlogi/smart_city_hub/entity/enums/Role.java
@@ -0,0 +1,11 @@
+package com.smartlogi.smart_city_hub.entity.enums;
+
+/**
+ * User roles in the Smart City Hub system.
+ */
+public enum Role {
+    ROLE_USER,       // Citizen
+    ROLE_AGENT,      // Municipal Agent
+    ROLE_SUPERVISOR, // Supervisor
+    ROLE_ADMIN       // Administrator
+}
