82a3c968f53b1bb2e00e806bfe0e36d3fea6d8f5
hamzalhadouchi <hamzabniss16@gmail.com>
2026-01-29 14:36:03 +0000

    add UpdateUserRequest class for updating user profile

---
+++ src/main/java/com/smartlogi/smart_city_hub/dto/request/UpdateUserRequest.java
@@ -0,0 +1,32 @@
+package com.smartlogi.smart_city_hub.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Request DTO for updating user profile.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserRequest {
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phone;
+
+    private Boolean active;
+}
