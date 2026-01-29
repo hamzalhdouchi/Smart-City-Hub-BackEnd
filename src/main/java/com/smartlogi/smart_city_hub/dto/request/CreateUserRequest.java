960b5f9d74fd8973b942897561e66b1fc7c58860
hamzalhadouchi <hamzabniss16@gmail.com>
2026-01-29 14:21:29 +0000

    add CreateUserRequest DTO for user creation

---
+++ src/main/java/com/smartlogi/smart_city_hub/dto/request/CreateUserRequest.java
@@ -0,0 +1,40 @@
+package com.smartlogi.smart_city_hub.dto.request;
+
+import com.smartlogi.smart_city_hub.entity.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Request DTO for creating a new user (Admin operation).
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUserRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    private String phone;
+
+    @NotNull(message = "Role is required")
+    private Role role;
+}
