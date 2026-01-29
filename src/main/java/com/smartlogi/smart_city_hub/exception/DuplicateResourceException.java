8fb5b749a2de58b0f0da4048cb3b12d1316ef3f8
hamzalhadouchi <hamzabniss16@gmail.com>
2026-01-29 14:22:32 +0000

    add DuplicateResourceException for handling duplicate resource creation

---
+++ src/main/java/com/smartlogi/smart_city_hub/exception/DuplicateResourceException.java
@@ -0,0 +1,19 @@
+package com.smartlogi.smart_city_hub.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Exception thrown when attempting to create a duplicate resource.
+ */
+@ResponseStatus(HttpStatus.CONFLICT)
+public class DuplicateResourceException extends RuntimeException {
+
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+
+    public DuplicateResourceException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s already exists with %s: '%s'", resourceName, fieldName, fieldValue));
+    }
+}
