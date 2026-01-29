da385584e26a2da0ea48bd713afcfbe6265e123d
hamzalhadouchi <hamzabniss16@gmail.com>
2026-01-29 14:21:19 +0000

    add CreateRatingRequest DTO for rating submission

---
+++ src/main/java/com/smartlogi/smart_city_hub/dto/request/CreateRatingRequest.java
@@ -0,0 +1,23 @@
+package com.smartlogi.smart_city_hub.dto.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateRatingRequest {
+    
+    @NotNull(message = "Stars rating is required")
+    @Min(value = 1, message = "Minimum rating is 1")
+    @Max(value = 5, message = "Maximum rating is 5")
+    private Integer stars;
+    
+    private String feedback;
+}
