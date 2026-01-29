f96925375b81738fc7b1dbd55e3949fa1635f71e
hamzalhadouchi <hamzabniss16@gmail.com>
2026-01-29 14:26:38 +0000

    add NotificationSimpleResponse DTO for minimal notification information

---
+++ src/main/java/com/smartlogi/smart_city_hub/dto/response/NotificationSimpleResponse.java
@@ -0,0 +1,25 @@
+package com.smartlogi.smart_city_hub.dto.response;
+
+import com.smartlogi.smart_city_hub.entity.enums.NotificationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * Simple response DTO for notification (minimal info).
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationSimpleResponse {
+
+    private Long id;
+    private String title;
+    private NotificationType type;
+    private Boolean read;
+    private LocalDateTime createdAt;
+}
