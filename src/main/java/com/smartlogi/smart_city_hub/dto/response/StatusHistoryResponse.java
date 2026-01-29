c9a987de6b3f1cdf70a7b4f7cdf9c97968dd7ca1
hamzalhadouchi <hamzabniss16@gmail.com>
2026-01-29 14:34:40 +0000

    add StatusHistoryResponse class for encapsulating incident status history entries

---
+++ src/main/java/com/smartlogi/smart_city_hub/dto/response/StatusHistoryResponse.java
@@ -0,0 +1,26 @@
+package com.smartlogi.smart_city_hub.dto.response;
+
+import com.smartlogi.smart_city_hub.entity.enums.IncidentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * Response DTO for incident status history entry.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StatusHistoryResponse {
+
+    private Long id;
+    private IncidentStatus previousStatus;
+    private IncidentStatus newStatus;
+    private String comment;
+    private String changedByName;
+    private LocalDateTime changedAt;
+}
