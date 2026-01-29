b9a24e4317543cd9b74397f77f25d4accde9cde9
hamzalhadouchi <hamzabniss16@gmail.com>
2026-01-29 14:36:16 +0000

    add UrgencyLevel enum for incident urgency classification

---
+++ src/main/java/com/smartlogi/smart_city_hub/entity/enums/UrgencyLevel.java
@@ -0,0 +1,11 @@
+package com.smartlogi.smart_city_hub.entity.enums;
+
+/**
+ * Urgency levels for incidents in the Smart City Hub system.
+ */
+public enum UrgencyLevel {
+    LOW,
+    MEDIUM,
+    HIGH,
+    CRITICAL
+}
