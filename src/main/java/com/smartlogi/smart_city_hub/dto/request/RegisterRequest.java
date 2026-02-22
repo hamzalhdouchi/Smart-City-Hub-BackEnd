bfef6950471b865c52130d4fb14362c9dcf27f6f
hamzalhadouchi <hamzabniss16@gmail.com>
2026-02-22 20:36:34 +0000

    refactor RegisterRequest to add nationalId field with validation and remove password requirement

--- src/main/java/com/smartlogi/smart_city_hub/dto/request/RegisterRequest.java
+++ src/main/java/com/smartlogi/smart_city_hub/dto/request/RegisterRequest.java
@@ -2,31 +2,36 @@ package com.smartlogi.smart_city_hub.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Request DTO for user registration.
+ * Password is not required - it will be generated upon admin approval.
+ */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
-    
+
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
     private String email;
-    
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters")
-    private String password;
-    
+
     @NotBlank(message = "First name is required")
     private String firstName;
-    
+
     @NotBlank(message = "Last name is required")
     private String lastName;
-    
+
+    @NotBlank(message = "Phone number is required")
     private String phone;
+
+    @NotBlank(message = "National ID is required")
+    @Pattern(regexp = "^[A-Za-z0-9]{5,20}$", message = "National ID must be 5-20 alphanumeric characters")
+    private String nationalId;
 }
