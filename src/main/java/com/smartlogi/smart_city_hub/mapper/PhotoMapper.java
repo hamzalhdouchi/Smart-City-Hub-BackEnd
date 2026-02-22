23ec570758179da8315ebf5e06a5c4ded1da3380
hamzalhadouchi <hamzabniss16@gmail.com>
2026-02-22 20:34:57 +0000

    refactor PhotoMapper to use IncidentPhoto and add MinioService for URL generation

--- src/main/java/com/smartlogi/smart_city_hub/mapper/PhotoMapper.java
+++ src/main/java/com/smartlogi/smart_city_hub/mapper/PhotoMapper.java
@@ -1,14 +1,21 @@
 package com.smartlogi.smart_city_hub.mapper;
 
 import com.smartlogi.smart_city_hub.dto.response.PhotoResponse;
-import com.smartlogi.smart_city_hub.entity.Photo;
+import com.smartlogi.smart_city_hub.entity.IncidentPhoto;
+import com.smartlogi.smart_city_hub.service.MinioService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface PhotoMapper {
-    
+public abstract class PhotoMapper {
+
+    @Autowired
+    protected MinioService minioService;
+
+    @Mapping(target = "url", expression = "java(minioService.getPresignedUrlSafe(photo.getFilePath()))")
     @Mapping(target = "uploadedById", source = "uploadedBy.id")
     @Mapping(target = "uploadedByName", source = "uploadedBy.fullName")
-    PhotoResponse toResponse(Photo photo);
+    @Mapping(target = "createdAt", source = "uploadedAt")
+    public abstract PhotoResponse toResponse(IncidentPhoto photo);
 }
