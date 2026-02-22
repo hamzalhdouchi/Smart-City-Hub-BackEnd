fd56a79f67a431dc41108d0578dd0061267b7222
hamzalhadouchi <hamzabniss16@gmail.com>
2026-02-22 20:32:14 +0000

    refactor CommentMapper to use ProfilePhotoService for author photo URL resolution

--- src/main/java/com/smartlogi/smart_city_hub/mapper/CommentMapper.java
+++ src/main/java/com/smartlogi/smart_city_hub/mapper/CommentMapper.java
@@ -2,13 +2,30 @@ package com.smartlogi.smart_city_hub.mapper;
 
 import com.smartlogi.smart_city_hub.dto.response.CommentResponse;
 import com.smartlogi.smart_city_hub.entity.Comment;
+import com.smartlogi.smart_city_hub.service.ProfilePhotoService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface CommentMapper {
-    
+public abstract class CommentMapper {
+
+    @Autowired
+    protected ProfilePhotoService profilePhotoService;
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "content", source = "content")
+    @Mapping(target = "createdAt", source = "createdAt")
+    @Mapping(target = "updatedAt", source = "updatedAt")
     @Mapping(target = "authorId", source = "author.id")
     @Mapping(target = "authorName", source = "author.fullName")
-    CommentResponse toResponse(Comment comment);
+    @Mapping(target = "authorRole", expression = "java(comment.getAuthor().getRole().name())")
+    @Mapping(target = "authorPhotoUrl", expression = "java(getAuthorPhotoUrl(comment.getAuthor().getId()))")
+    public abstract CommentResponse toResponse(Comment comment);
+
+    @Named("resolvePhotoUrl")
+    protected String getAuthorPhotoUrl(String userId) {
+        return profilePhotoService.getCurrentProfilePhotoUrl(userId).orElse(null);
+    }
 }
