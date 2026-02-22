2421c5fc84a201dd6c46db5002461c490fe46903
hamzalhadouchi <hamzabniss16@gmail.com>
2026-02-22 20:32:59 +0000

    refactor CreatePhotoRequest to use String for incidentId and update validation

--- src/main/java/com/smartlogi/smart_city_hub/dto/request/CreatePhotoRequest.java
+++ src/main/java/com/smartlogi/smart_city_hub/dto/request/CreatePhotoRequest.java
@@ -1,23 +1,19 @@
 package com.smartlogi.smart_city_hub.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Request DTO for photo upload metadata.
- */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreatePhotoRequest {
 
-    @NotNull(message = "Incident ID is required")
-    private Long incidentId;
+    @NotBlank(message = "Incident ID is required")
+    private String incidentId;
 
     @NotBlank(message = "File name is required")
     private String fileName;
